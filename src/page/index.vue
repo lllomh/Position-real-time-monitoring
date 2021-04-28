0cedf441c08fc1c5b6d4b79008023ff8e016952f
lllomh <lllomh@qq.com>
2021-04-28 11:25:37 +0800

    'githubWatch'

--- src/page/index.vue
+++ src/page/index.vue
@@ -15,14 +15,14 @@
                         </div>
                         <div class="titles">数据来源: 各大主流招聘平台</div>
                         <div class="github">
-                           <span class="ff" style="margin-left: 115px">
+                           <span class="ff">
                                 <a href="https://github.com/lllomh/Position-real-time-monitoring" target="_blank"><img src="https://img.shields.io/github/stars/lllomh/Position-real-time-monitoring?style=social" alt=""/></a>
                             </span>
                             <span class="ff yu" style="margin-left: 5px">
                                 <a href="https://github.com/lllomh/Position-real-time-monitoring" target="_blank"> <img src="https://img.shields.io/github/forks/lllomh/Position-real-time-monitoring?style=social" alt=""/></a>
                             </span>
                             <span class="ff wu" style="margin-left: 5px">
-                                <a href="https://github.com/lllomh/Position-real-time-monitoring" target="_blank"><iframe id="wacth" src="https://ghbtns.com/github-btn.html?user=lllomh&repo=Position-real-time-monitoring&type=watch&count=true&v=2" frameborder="0" scrolling="0" width="200" height="20" title="GitHub"></iframe></a>
+                                <a href="https://github.com/lllomh/Position-real-time-monitoring" target="_blank"><img src="https://img.shields.io/github/watchers/lllomh/Position-real-time-monitoring?style=social" alt=""></a>
                             </span>
                         </div>
                     </div>
