0a333cc9ed28adf59df62c5167c80835584a553b
lllomh <lllomh@qq.com>
2021-11-29 11:24:26 +0800

    'space'

--- src/config/api.js
+++ src/config/api.js
@@ -8,7 +8,6 @@ if (process.env.NODE_ENV === "production") {
     // $api="http://www.api.com/index.php/" //test 本机
 }
 
-
 export const P_GET_CONT = $api+'frontapi/get_cont';
 export const P_GET_CONTS = $api+'frontapi/get_conts';
 export const P_GET_ALL = $api+'frontapi/get_conts_all';
